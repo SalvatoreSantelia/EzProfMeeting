5ff2fe6f66d36c0841ecd56bb0fa0d6992ac2c69
salvo966 <fdr_official@live.it>
2018-12-13 18:29:33 +0100

    Classe di prova per il testing

---
+++ src/ClasseDiProva.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClasseDiProva {
+
+
+
+    public ClasseDiProva(){}
+    private int count=0;
+    public static String verify(String s){
+
+return null;
+        }
+    }
+
+
+
--- web/index.jsp
+++ web/index.jsp
@@ -8,9 +8,9 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
-    <title>$Title$</title>
+    <title>Albero</title>
   </head>
   <body>
-  $END$
+  tronco
   </body>
 </html>
