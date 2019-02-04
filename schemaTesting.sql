84abb9aea68f66119d5c9e5a5f0956cb993a5683
salvo966 <fdr_official@live.it>
2019-02-04 19:10:10 +0100

    Cambio schema

--- schemaTesting.sql
+++ schemaTesting.sql
@@ -40,7 +40,7 @@ CREATE TABLE `login` (
 
 LOCK TABLES `login` WRITE;
 /*!40000 ALTER TABLE `login` DISABLE KEYS */;
-INSERT INTO `login` VALUES ('mia@email.it','miapassword2','studente'),('miaemail','miapassword','studente'),('prof@prof.it', 'profprofprof', 'professore');
+INSERT INTO `login` VALUES ('mia@email.it','miapassword2','studente'),('miaemail','miapassword','studente'),('prof@prof.it','profprofprof','professore');
 /*!40000 ALTER TABLE `login` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -64,7 +64,7 @@ CREATE TABLE `messaggio` (
   KEY `idStudente_idx` (`idStudente`),
   CONSTRAINT `idProfessore` FOREIGN KEY (`idProfessore`) REFERENCES `professore` (`idProfessore`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `idStudente` FOREIGN KEY (`idStudente`) REFERENCES `studente` (`idStudente`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -73,7 +73,7 @@ CREATE TABLE `messaggio` (
 
 LOCK TABLES `messaggio` WRITE;
 /*!40000 ALTER TABLE `messaggio` DISABLE KEYS */;
-INSERT INTO `messaggio` VALUES (1,'2019-12-12','ciao',2,2,'studente','12:12:24.000000'),(2,'2019-12-12','ciao',1,1,'professore','14:23:59.000000'),(3,'2019-11-10','hey',1,2,'stuente','17:18:35.000000'),(4,'2019-01-28','CIAOPROF',1,1,'studente','11:28:33.000000'),(5,'2019-01-28','CIAOPROF',1,1,'studente','15:37:20.000000'),(6,'2019-01-28','CIAOPROF',1,1,'studente','16:11:54.000000'),(7,'2019-01-28','CIAOPROF',1,1,'studente','16:58:57.000000');
+INSERT INTO `messaggio` VALUES (1,'2019-12-12','ciao',2,2,'studente','12:12:24.000000'),(2,'2019-12-12','ciao',1,1,'professore','14:23:59.000000'),(3,'2019-11-10','hey',1,2,'stuente','17:18:35.000000'),(4,'2019-01-28','CIAOPROF',1,1,'studente','11:28:33.000000'),(5,'2019-01-28','CIAOPROF',1,1,'studente','15:37:20.000000'),(6,'2019-01-28','CIAOPROF',1,1,'studente','16:11:54.000000'),(7,'2019-01-28','CIAOPROF',1,1,'studente','16:58:57.000000'),(8,'2019-02-04','CIAOPROF',1,1,'studente','15:46:53.000000'),(9,'2019-02-04','CIAOPROF',1,1,'studente','15:47:35.000000'),(10,'2019-02-04','CIAOPROF',1,1,'studente','15:48:45.000000'),(11,'2019-02-04','CIAOPROF',1,1,'studente','16:06:08.000000'),(12,'2019-02-04','CIAOPROF',1,1,'studente','16:13:08.000000'),(13,'2019-02-04','CIAOPROF',1,1,'studente','16:15:58.000000'),(14,'2019-02-04','CIAOPROF',1,1,'studente','16:17:28.000000'),(15,'2019-02-04','CIAOPROF',1,1,'studente','16:22:07.000000'),(16,'2019-02-04','CIAOPROF',1,1,'studente','16:23:28.000000'),(17,'2019-02-04','CIAOPROF',1,1,'studente','16:26:23.000000'),(18,'2019-02-04','CIAOPROF',1,1,'studente','16:29:23.000000'),(19,'2019-02-04','CIAOPROF',1,1,'studente','16:37:00.000000'),(20,'2019-02-04','CIAOPROF',1,1,'studente','16:38:50.000000'),(21,'2019-02-04','CIAOPROF',1,1,'studente','16:44:40.000000'),(22,'2019-02-04','CIAOPROF',1,1,'studente','16:47:29.000000'),(23,'2019-02-04','CIAOPROF',1,1,'studente','17:24:21.000000'),(24,'2019-02-04','CIAOPROF',1,1,'studente','17:26:33.000000'),(25,'2019-02-04','CIAOPROF',1,1,'studente','17:29:03.000000'),(26,'2019-02-04','CIAOPROF',1,1,'studente','17:45:24.000000'),(27,'2019-02-04','CIAOPROF',1,1,'studente','18:20:01.000000'),(28,'2019-02-04','CIAOPROF',1,1,'studente','18:45:13.000000'),(29,'2019-02-04','CIAOPROF',1,1,'studente','18:55:23.000000'),(30,'2019-02-04','CIAOPROF',1,1,'studente','18:57:13.000000');
 /*!40000 ALTER TABLE `messaggio` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -97,7 +97,7 @@ CREATE TABLE `prenotazione` (
   KEY `idStud_idx` (`idStudente`),
   CONSTRAINT `idRicevimento` FOREIGN KEY (`idRicevimento`) REFERENCES `ricevimento` (`idRicevimento`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `idStud` FOREIGN KEY (`idStudente`) REFERENCES `studente` (`idStudente`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -125,7 +125,7 @@ CREATE TABLE `professore` (
   `telefonoProfessore` varchar(45) DEFAULT NULL,
   `ufficioProfessore` varchar(45) NOT NULL,
   PRIMARY KEY (`idProfessore`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -134,7 +134,7 @@ CREATE TABLE `professore` (
 
 LOCK TABLES `professore` WRITE;
 /*!40000 ALTER TABLE `professore` DISABLE KEYS */;
-INSERT INTO `professore` VALUES (1,'Filomena','Ferrucci','fferrucci@unisa.it','0819876543','stecca F'),(2,'Carmine','Gravino','cgravino@unisa.it','0819876543','stecca F'), (3, 'Mamma', 'Papa', 'prof@prof.it','666', 'ufficio 21');
+INSERT INTO `professore` VALUES (1,'Filomena','Ferrucci','fferrucci@unisa.it','0819876543','stecca F'),(2,'Carmine','Gravino','cgravino@unisa.it','0819876543','stecca F'),(3,'Mamma','Papa','prof@prof.it','666','ufficio 21');
 /*!40000 ALTER TABLE `professore` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -151,13 +151,13 @@ CREATE TABLE `ricevimento` (
   `orarioFine` time NOT NULL,
   `luogo` varchar(80) NOT NULL,
   `data` date NOT NULL,
-  `postiTotali` int NOT NULL,
-  `postiDisponibili` int NOT NULL,
+  `postiTotali` int(11) NOT NULL,
+  `postiDisponibili` int(11) NOT NULL,
   `idProfessore` int(11) DEFAULT NULL,
   PRIMARY KEY (`idRicevimento`),
   KEY `idProfessore_idx` (`idProfessore`),
   CONSTRAINT `idProf` FOREIGN KEY (`idProfessore`) REFERENCES `professore` (`idProfessore`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -166,7 +166,7 @@ CREATE TABLE `ricevimento` (
 
 LOCK TABLES `ricevimento` WRITE;
 /*!40000 ALTER TABLE `ricevimento` DISABLE KEYS */;
-INSERT INTO `ricevimento` VALUES (1,'12:00:00','12:30:00','stecca F','2019-02-16', 2, 0,1),(2,'12:00:00','12:30:00','stecca F','2019-02-16', 2, 0, 2),(3,'11:12:38','11:31:40','stecca F','2018-12-12', 2, 0,2),(4,'11:12:38','11:31:40','stecca F','2018-12-12', 2, 0, 2);
+INSERT INTO `ricevimento` VALUES (1,'12:00:00','12:30:00','stecca F','2019-02-16',2,0,1),(2,'12:00:00','12:30:00','stecca F','2019-02-16',2,0,2),(3,'11:12:38','11:31:40','stecca F','2018-12-12',2,0,2),(4,'11:12:12','11:31:40','stecca F','2018-12-12',2,0,2);
 /*!40000 ALTER TABLE `ricevimento` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -186,7 +186,7 @@ CREATE TABLE `studente` (
   `telefonoStudente` varchar(45) DEFAULT NULL,
   `numAssenza` int(11) DEFAULT '0',
   PRIMARY KEY (`idStudente`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -195,7 +195,7 @@ CREATE TABLE `studente` (
 
 LOCK TABLES `studente` WRITE;
 /*!40000 ALTER TABLE `studente` DISABLE KEYS */;
-INSERT INTO `studente` VALUES (1,'Salvatore','Santelia','0512104001','s.santelia1@studenti.unisa.it','3334455678',15),(2,'Rocco','Aliberti','0512104627','r.aliberti18@studenti.unisa.it','3334455678',1);
+INSERT INTO `studente` VALUES (1,'Salvatore','Santelia','0512104001','s.santelia1@studenti.unisa.it','3334455678',48),(2,'Rocco','Aliberti','0512104627','r.aliberti18@studenti.unisa.it','3334455678',1);
 /*!40000 ALTER TABLE `studente` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -208,4 +208,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-01-28 17:00:50
+-- Dump completed on 2019-02-04 19:08:48
